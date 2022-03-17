ed80c51cfad444ab439f9534d5f09b481743afd2
Jeremias Marinho <jeremias10@msn.com>
2022-03-17 04:57:35 -0300

    criação dos formularios

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -1,13 +1,16 @@
 interface BotaoProps {
  
+  cor: 'red' | 'blue' | 'green'
   className?: string
   children: any
+  onClick?: () => void
 }
 
-export default function Botao(props) {
+export default function Botao(props: BotaoProps) {
+  const cor = props.cor ?? 'green'
   return (
-    <button className={`
-      bg-gradient-to-r from-orange-700 to-orange-500
+    <button onClick={props.onClick} className={`
+      bg-gradient-to-r from-${cor}-400 to-${cor}-700
       text-slate-100 px-4 py-2 rounded-md
       ${props.className}
     `}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,56 @@
+import { useState } from "react";
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 
 export default function Home() {
-
   const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 21, '2'),
-    new Cliente('João', 18, '3'),
-    new Cliente('Carlos', 41, '4'),
-  ]
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 21, "2"),
+    new Cliente("João", 18, "3"),
+    new Cliente("Carlos", 41, "4"),
+  ];
 
   function clienteSelecionado(cliente: Cliente) {
-      console.log(cliente.nome)
+    console.log(cliente.nome);
   }
   function clienteExcluido(cliente: Cliente) {
-    console.log(cliente.nome)
+    console.log(cliente.nome);
   }
 
+  const [visivel, setVisivel] = useState<"tabela" | "form">("tabela");
+
   return (
-    <div className={`
+    <div
+      className={`
       flex h-screen justify-center items-center
       bg-gradient-to-b from-slate-300 to-slate-600
       text-slate-900
-    `}>
-     <Layout titulo="Cadastro Simples">
-       <div className="flex justify-end"> 
-          <Botao className="mb-4">Novo Cliente</Botao>
-       </div>
-       <Tabela clientes={clientes} 
-       clienteSelecionado={clienteSelecionado}
-       clienteExcluido={clienteExcluido}
-       ></Tabela>
-     </Layout>
+    `}
+    >
+      <Layout titulo="Cadastro Simples">
+        {visivel === "tabela" ? (
+          <>
+            <div className="flex justify-end">
+              <Botao cor="red" className="mb-4" onClick={() => setVisivel('form')}>
+                Novo Cliente
+              </Botao>
+            </div>
+            <Tabela
+              clientes={clientes}
+              clienteSelecionado={clienteSelecionado}
+              clienteExcluido={clienteExcluido}
+            ></Tabela>
+          </>
+        ) : (
+          <Formulario 
+              cliente={clientes[3]} 
+              cancelado={() => setVisivel('tabela')}
+              ></Formulario>
+        )}
+      </Layout>
     </div>
-  )
+  );
 }
